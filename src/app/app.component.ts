c5d12603abb3a539067c0430e18d15b58ee82cf1
Marco Torres <marcoantonio.torres@icloud.com>
2024-04-03 15:20:09 -0400

    In version 3 we focus on showing an error message if the user has not entered a valid value or not using a reactive form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,68 +36,50 @@ export class AppComponent implements OnInit{
   }
 
   // SIDE NOTES - FORM VALIDATION IN REACTIVE FORM
-  // In version 2 - This version will focus on how we can perform validation on the form controls of a reactive form
+  // In version 3, we will focus on how to show a validation error message to the user, if the user has not entered a valid value in a 
+  // form control
 
-  // Originally to add validation in the HTML we would use attributes such as required to make a field require user input 
-  // and email in order to have the user 
-
-  // In the case of a Reactive Form, we don't add these HTML attribute validators on the form elements/HTML, we add it on the 
-  // form controls which are defined inside the FormGroup above. 
-  // We use Validators (this is provided by angular forms) which gives us a variety of different methods to implement different validations
-    // Ex. firstname: new FormControl(null, Validators.required),
-    // Where Validators.required makes the form control a required field
-
-  // Currently we do this for firstname, lastname, and email 
-  // In doing so if we submit the form without providing the required fields the form will be invalid (ie valid:false, invalid: true)
-
-
-  // HOWEVER, there is an issue with this even when providing input. Email will take any user input and mark it as valid 
-  // To fix this we need to add another validation to our email form control.
-  // When using multiple validators on a form control we need to use an array and inside we specify the validators we want to use 
-    // Ex. email: new FormControl(null, [Validators.required, Validators.email]),
+  // First we want to add some small text that will appear in the template
+    // Ex. <small>*First Name is a required field.</small>
+    // This already contains some css, which makes the color red and font bold
   
-  // Now when submitting an invalid email address, the entire form will be marked as invalid 
-  // We also now have access to see what validation error has occurred on this form control
-  // There is an errors property
-    // Ex. errors: {email: true}
-    // meaning that we do have an error code coming from the email
 
-  // If we enter a valid email address on the control level we see 
-    // Ex. errors: null
-
+  // PROBLEM - Currently this text is static, meaning that even if we enter a valid value the text will still be there
+  // SOLUTION - We access the firstname control from the contols property. firstname control will contain its own valid/invalid property
   
-  // Moving on to the state of the form and using classes to get said state
-  // Just like before. Angular adds classes on the form depending on whether the user has interacted or if the form is valid 
-  // If we touch an input the class ng-untouched will be removed amd ng-touched will be added
-  // If we provide a valid value... the ng-invalid class will be removed and ng-valid will be added 
-  // Same concept with ng-prestine and ng-dirty once the inputs have been touched 
-
-  // Keep in mind that if any control in the form is touched, that means the complete form is touched
-  // If any for control is dirty, that means the complete form is dirty
-  // And if any form control has an invalid value, that means the complete form is invalid 
-
-  // So we can manipulate our form based on this reaction
-
-
-  // SHOWING A RED BORDER AROUND THE FORM
-  // In the css we add .ng-invalid {border: red 2px solid;}
-    // Meaning if ng-invalid appears in the class then set the border to red 
-    // If it does not appear that means that the border is being overwritten by another class 
-
-  // PROBLEM This will however make the entire form border red on top of the inputs 
-  // SOLUTION - We specify that we want the border to appear on inputs 
-    // Ex. input.ng-invalid {border: red 2px solid;}
-
-  // PROBLEM 2 - We only want the red border to show when the user has touched the input (currently it's aways on)
-  // SOLUTION - We use ng-touched to dictate when the user has interacted with the form
-    // Ex. input.ng-invalid.ng-touched {border: red 2px solid;}
-    // Now the border will only appear when we click inside and click outside without adding a valid input
+  // To acomplish this we use get method that goes with FormGroup.
+  // This method allows us to access a form control from a FormGroup
   
-  // Remember there are more Validators available, ie min, max...
+  // First - We create an ngIf directive that will show or not show the text depending on a boolean value
+  // Second - We use reactiveForm in the directive since that is where our FormGroup lives
+  // Third - We call the get method and pass a string value (being the name of the form control) in this case firstname
+          // This will return us the first name form control from this reactive form group
+  // Forth - we use the invalid property (If the invalid prop is true - then small element is rendered)
   
-  
+    // Ex. <small *ngIf="reactiveForm.get('firstname')?.invalid"> text <small> 
+  // Now when we enter a valid value we see the red test removed 
+
+
+  // PROBLEM - When the page loads the red text will still be there
+  // SOLUTION - We can use touched property of form control 
+
+  // First - We add an && operator to our directive 
+  // Second -  We again use get method and access the touched property of firstname
+
+    // Ex. reactiveForm.get('firstname')?.touched
+
+
+  // We can now repeat these steps wherever we want this validation to show up, we just need to replace the property we want to retrieve 
+
+
+  // NOW, we want to disable the submit button if any of the form controls are invalid, else we can submit
+  // We use the form level valid and invalid property to control this
+
+  // To achieve this we use the disabled attribute and use property binding
+  // We again use the reactiveForm since that's where our FormGroup lives
   
 
 
 
+  
 }
